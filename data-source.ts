368f29bb74bda6ea8e5fffde71e9276c111e7d0b
Chinweike99 <innocentakwolu1@gmail.com>
2026-02-01 11:34:18 +0100

    fix: fails

--- data-source.ts
+++ data-source.ts
@@ -1,5 +1,4 @@
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
-import { typeOrmConfig } from './src/config/database/db.config';
-
+import { typeOrmConfig } from './src/config/database/db.config.js';
 export const AppDataSource = new DataSource(typeOrmConfig);
