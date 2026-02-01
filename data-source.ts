026f4d47e47fb20dca649b896ca10a81da535c31
Chinweike99 <innocentakwolu1@gmail.com>
2026-02-01 11:15:03 +0100

    chore: setup ci/cd

--- data-source.ts
+++ data-source.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
-import { typeOrmConfig } from './src/database/db.config';
+import { typeOrmConfig } from './src/config/database/db.config';
 
 export const AppDataSource = new DataSource(typeOrmConfig);
