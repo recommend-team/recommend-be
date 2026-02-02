af395280ef87ac8a03fb9fa84e98462a1ab95301
Chinweike99 <innocentakwolu1@gmail.com>
2026-02-02 19:35:36 +0100

    chore: updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { ScheduleModule } from '@nestjs/schedule';
+import { BullModule } from '@nestjs/bull';
 import { AppConfigModule } from './config/config.module';
 import { RedisModule } from './common/redis/redis.module';
+import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './config/database/database.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     AppConfigModule,
     DatabaseModule,
     RedisModule,
+    CommonModule,
+    AuthModule,
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
     ThrottlerModule.forRoot({
       ttl: 60,
       limit: 100,
